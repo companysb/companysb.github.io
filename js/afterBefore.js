a78158e187a649d2152309a24a232170564f630b
Roman Balan <texxnic@gmail.com>
2015-06-19 20:12:28 +0300

    1ver
    1ver

---
+++ js/afterBefore.js
@@ -0,0 +1,12 @@
+var $black_white = $('.black_white'),
+		img_width = $('.black_white img').width(),
+		init_split = Math.round(img_width/2);
+  
+  $black_white.width(init_split);  
+
+		$('.before_after_slider').mousemove(function(e){
+		var offX  = (e.offsetX || e.clientX - $black_white.offset().left);
+			$black_white.width(offX);
+		});
+
+		
